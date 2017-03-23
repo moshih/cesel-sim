3284221a435673222273681210e56b8db0b76996
Kevin Kiningham <kkiningh@stanford.edu>
2017-03-22 17:10:51 -0700

    Add initial version of CESEL

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-Instruction Set simulator for CESEL.
+#Instruction Set simulator for CESEL#
 
 Author: Kevin Kiningham
+
+## How to use ##
+
+To create a new program:
+
+```python
+p = cesel.Program('Program Name')
+p.add(1, 2, 3) # Add register 2 to register 3 and store in register 1
+# etc
+```
+
+To simulate a program:
+
+```python
+# Create a new Interpreter with the register file initialized to all ones
+s = cesel.Interpreter(program, regfile=1)
+
+# Step the simulation
+s.step()
+
+# See the value of the first register
+print s.regfile[0]
+```
---
+++ cesel.py
@@ -0,0 +1,241 @@
+import numpy as np
+
+# Instruction constants
+INSTR_BITS = 16
+
+# Total number of lanes
+N_LANES = 32
+
+# Registerfile constants
+REGFILE_N_REGS    = 16
+REGFILE_DATA_BITS = 8
+REGFILE_ADDR_BITS = np.uint(np.ceil(np.log2(REGFILE_N_REGS)))
+
+# EX stage constants
+EX_OP_BITS     = 4
+
+EX_OP_XOR       = 0b0000
+EX_OP_AND       = 0b0001
+EX_OP_XNOR      = 0b0010
+EX_OP_ROR       = 0b0011
+EX_OP_ADD       = 0b0100
+EX_OP_SUB       = 0b0101
+EX_OP_MUL       = 0b0110
+EX_OP_GF2       = 0b0111
+EX_OP_BITSLICE  = 0b1000
+EX_OP_PERMUTE   = 0b1001
+EX_OP_SBOX      = 0b1010
+EX_OP_RESERVED1 = 0b1100 # Load immediate
+EX_OP_RESERVED2 = 0b1101
+EX_OP_RESERVED3 = 0b1110
+EX_OP_RESERVED4 = 0b1111 # Extended Instr
+
+# Rotate/shift
+# Load immediate
+# Pop Message/Key
+# Push output
+# Bitmatmul?
+# wide mult/add/sub?
+# Loops?
+# Masking?
+
+def instr_r16(op, rw, r1, r0):
+    """Insert a 16-bit R-type instruction"""
+    assert op & 0xFF == op, "op must be an 8-bit value"
+    assert rw & 0xFF == rw, "rw must be an 8-bit value"
+    assert r1 & 0xFF == r1, "r1 must be an 8-bit value"
+    assert r0 & 0xFF == r0, "r0 must be an 8-bit value"
+
+    # R16-type instruction
+    # 4 bits | 4 bits | 4 bits | 4 bits
+    # Ex Op  | r1     | r0     | rw
+    return (op << 12) | (r1 << 8) | (r0 << 4) | (rw << 0)
+
+def decode(instr):
+    # Check if it's an R16-type instruction
+    assert (instr & 0xF000) >> 12 != 0xF, "can only decode R-type instructions"
+
+    # Simple decode
+    op = (instr & 0xF000) >> 12
+    rw = (instr & 0x000F) >> 0
+    r1 = (instr & 0x0F00) >> 8
+    r0 = (instr & 0x00F0) >> 4
+
+    # Write is always enabled for now
+    write_en = 1
+
+    return op, rw, write_en, r1, r0
+
+def execute(op, v1, v0, acc):
+    vw = np.zeros(shape=acc.shape, dtype=acc.dtype)
+    if op == EX_OP_XOR:
+        vw = v1 ^ v0
+    elif op == EX_OP_AND:
+        vw = v1 & v0
+    elif op == EX_OP_XNOR:
+        vw = ~(v1 ^ v0)
+    elif op == EX_OP_ROR:
+        vw = (v1 >> v0) | (v1 << 8 - v0)
+    elif op == EX_OP_ADD:
+        vw = v1 + v0
+    elif op == EX_OP_SUB:
+        vw = v1 - v0
+    elif op == EX_OP_MUL:
+        vw = v1 * v0
+    elif op == EX_OP_BITSLICE:
+        # Break out each
+        bits = np.unpackbits(np.array([0x1 for _ in range(8)]))
+        # TODO
+    elif op == EX_OP_PERMUTE:
+        # TODO
+        pass
+    else:
+        raise Exception("Illegal Instruction!")
+
+    return vw, acc
+
+class Program(object):
+    """Container for the asm program"""
+    def __init__(self, name='<unnamed>', code=[]):
+        self.name = name
+        self.code = code
+
+    def xor(self, rw, r1, r0):
+        self.code.append(instr_r16(EX_OP_XOR, rw, r1, r0))
+
+    def and_(self, rw, r1, r0):
+        self.code.append(instr_r16(EX_OP_AND, rw, r1, r0))
+
+    def xnor(self, rw, r1, r0):
+        self.code.append(instr_r16(EX_OP_XNOR, rw, r1, r0))
+
+    def ror(self, rw, r1, r0):
+        self.code.append(instr_r16(EX_OP_ROR, rw, r1, r0))
+
+    def add8(self, rw, r1, r0):
+        self.code.append(instr_r16(EX_OP_ADD, rw, r1, r0))
+
+    def sub8(self, rw, r1, r0):
+        self.code.append(instr_r16(EX_OP_SUB, rw, r1, r0))
+
+    def mul8(self, rw, r1, r0):
+        self.code.append(instr_r16(EX_OP_MUL, rw, r1, r0))
+
+    def bitslice(self, rw, r1, r0):
+        self.code.append(instr_r16(EX_OP_BITSLICE, rw, r1, r0))
+
+    def permute(self, rw, r1, r0):
+        self.code.append(instr_r16(EX_OP_PERMUTE, rw, r1, r0))
+
+    def __str__(self):
+        return "Program(name='{}')".format(self.name)
+
+    def serialize(self, fmt="{:04x}\n"):
+        result = ""
+        for instr in self.code:
+            result += fmt.format(instr)
+
+        return result
+
+class Interpreter(object):
+    """Simple interpreter for a program"""
+    def __init__(self, program=None, state="running", pc=0, regfile=None):
+        # Default to an empty program
+        if program is None:
+            program = Program()
+
+        # Code
+        self.program = program
+
+        # Current instruction
+        self.pc = pc
+
+        # Register file
+        # several helpers to setup register file
+        regfile_shape = (REGFILE_N_REGS, N_LANES)
+        if regfile is None:
+            # Default to all zeros
+            self.regfile = np.zeros(shape=regfile_shape, dtype=np.uint8)
+
+        elif regfile is 'random' or regfile is 'rand':
+            # Random initialization
+            self.regfile = np.random.randint(256, size=regfile_shape, dtype=np.uint8)
+
+        elif isinstance(regfile, int):
+            # All values equal
+            self.regfile = np.full(shape=regfile_shape, fill_value=regfile, dtype=np.uint8)
+
+        elif isinstance(regfile, dict):
+            # Set specific registers equal to value using a dict
+            # e.g. {0: 0xFF, 1: 0x11} sets register 0 to 0xFF and register 1 to 0x11
+            self.regfile = np.zeros(shape=regfile_shape, dtype=np.uint8)
+            for k, v in regfile.viewitems():
+                self.regfile[k] = v
+
+        else:
+            # Just convert whatever crap they gave us to a numpy array
+            self.regfile = np.asarray(regfile, dtype=uint8, copy=True)
+
+            # Attempt to at least convert it to the right shape
+            self.regfile = np.broadcast_to(self.regfile, regfile_shape)
+
+        # Accumulator State
+        self.acc = np.zeros(shape=(1, N_LANES), dtype=np.uint16)
+
+        # Current "State" (one of "running" or "halted")
+        self.state = state
+
+    def step(self):
+        # Get the current instruction
+        instr = self.program.code[self.pc]
+
+        # Decode the instruction
+        op, rw, write_en, r1, r0 = decode(instr)
+
+        # Read the regfile
+        v0 = self.regfile[r0]
+        v1 = self.regfile[r1]
+
+        # Execute
+        try:
+            vw, self.acc = execute(op, v0, v1, self.acc)
+        except RuntimeError as e:
+            raise RuntimeError("Exception in {} at pc = {}: {}"
+                    .format(self.program, self.pc, e))
+
+        # Writeback
+        if write_en:
+            self.regfile[rw] = vw
+
+        # Advance to next instruction
+        self.pc += 1
+
+        return self.state
+
+    def run(self):
+        while self.state != "halted":
+            self.step()
+
+        return self.state
+
+    def __str__(self):
+        return "Interpreter(state='{}', pc={})".format(self.state, self.pc)
+
+def main(args=None):
+    # Simple proxies to make our code prettier
+    # The "Program" class only need integers
+    R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15 = np.arange(16)
+
+    # Program Definition
+    p = Program(regfile={i: i for i in range(16)})
+    p.xor(R0, R0, R0)
+    p.add8(R0, R0, R0)
+    p.mul8(R1, R1, R0)
+
+    # Interpreter
+    i = Interpeter(program=p)
+    i.step()
+    i.step()
+
+if __name__ == '__main__':
+    main()
