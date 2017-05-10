143c33b042f66a163ba124a0a267839060930f75
Maurice Shih <maurices@stanford.edu>
2017-05-10 03:30:52 -0700

    sigma_one works

--- cesel.py
+++ cesel.py
@@ -457,19 +457,42 @@ def sigma_one():
     p = Program()
     
     
-
-    #rotate by 5 to get the second part of sigmazero (rotate by 39 in total)
+    ##############################################################################
+    #rotate by 28 to get the first part of sigmazero
     
-    #set set R15 as the permutation block (3,0,1,2)
+    
     p.permute(R1,R15,R0)
-    #set set R15 as 3
+    #set set R15 as 2
     p.ror(R0,R15,R0)
     p.ror(R1,R15,R1)
-    #set set R15 as 0b00011111
+    #set set R15 as 0b00111111
     p.and_(R0,R0,R15)
-    #set set R15 as 0b11100000
+    #set set R15 as 0b11000000
     p.and_(R1,R1,R15)
     p.add8(R0,R1,R0)
+    
+    
+    p.permute(R1,R15,R0)
+    #set set R15 as 2
+    p.ror(R0,R15,R0)
+    p.ror(R1,R15,R1)
+    #set set R15 as 0b00111111
+    p.and_(R0,R0,R15)
+    #set set R15 as 0b11000000
+    p.and_(R1,R1,R15)
+    p.add8(R0,R1,R0)
+    
+    p.permute(R1,R15,R0)
+    p.permute(R0,R15,R0)
+    #set set R15 as 2
+    p.ror(R0,R15,R0)
+    p.ror(R1,R15,R1)
+    #set set R15 as 0b00111111
+    p.and_(R0,R0,R15)
+    #set set R15 as 0b11000000
+    p.and_(R1,R1,R15)
+    p.add8(R0,R1,R0)
+    
     
      ########-------------------------------------------------------------------------------
     # Interpreter
@@ -479,23 +502,59 @@ def sigma_one():
     print ([np.binary_repr(n, width=8) for n in i.regfile[0][0:4]])
     ##############################################################################
     #rotate by 28 to get the first part of sigmazero
-    i.regfile[15]=[1,2,3,0, 5,6,7,4, 9,10,11,8, 13,14,15,12, 17,18,19,16, 21,22,23,20,  25,26,27,24, 29,30,31,28]
-
-    #rotate by 5 to get the second part of sigmazero (rotate by 39 in total)
+    i.regfile[15]=[3,0,1,2, 7,4,5,6, 11,8,9,10, 15,12,13,14, 19,16,17,18, 23,20,21,22, 27,24,25,26, 31,28,29,30]
+    i.step()
+    i.regfile[15]=[0b00000110 for x in range(0,32)]
+    i.step()
+    i.step()
+    #i.regfile[15]=[0b00111111 for x in range(0,32)]
+    i.regfile[15]=[0b00000011 for x in range(0,32)]
+    i.step()
+    #i.regfile[15]=[0b11000000 for x in range(0,32)]
+    i.regfile[15]=[0b11111100 for x in range(0,32)]
+    i.step()
+    i.step()
+    print ([np.binary_repr(n, width=8) for n in i.regfile[0][0:4]])
+    sigmazero_one=i.regfile[0][0:4]
+    
+    
     i.regfile[15]=[3,0,1,2, 7,4,5,6, 11,8,9,10, 15,12,13,14, 19,16,17,18, 23,20,21,22, 27,24,25,26, 31,28,29,30]
     i.step()
     i.regfile[15]=[0b00000101 for x in range(0,32)]
     i.step()
     i.step()
+    #i.regfile[15]=[0b00111111 for x in range(0,32)]
     i.regfile[15]=[0b00000111 for x in range(0,32)]
-
-    
     i.step()
-    i.regfile[15]=[0b11111000      for x in range(0,32)]
+    #i.regfile[15]=[0b11000000 for x in range(0,32)]
+    i.regfile[15]=[0b11111000 for x in range(0,32)]
     i.step()
     i.step()
     print ([np.binary_repr(n, width=8) for n in i.regfile[0][0:4]])
-    #sigmazero_three=i.regfile[0][8:12]
+    sigmazero_two=i.regfile[0][4:8]
+    
+    
+    
+    ##############################################################################
+    #4,5,6,7, 8,9,10,11, 12,13,14,15, 16,17,18,19, 20,21,22,23, ,24,25,26,27, 28,29,30,31
+    #rotate by 28 to get the first part of sigmazero
+    i.regfile[15]=[2,3,0,1, 6,7,4,5  ,10,11,8,9 ,14,15,12,13 ,18,19,16,17 ,22,23,20,21 ,26,27,24,25 ,30,31,28,29]
+    i.step()
+    i.regfile[15]=[3,0,1,2,  7,4,5,6, 11,8,9,10, 15,12,13,14, 19,16,17,18, 23,20,21,22, 27,24,25,26, 31,28,29,30]
+    i.step()
+    i.regfile[15]=[0b00000110 for x in range(0,32)]
+    i.step()
+    i.step()
+    #i.regfile[15]=[0b00111111 for x in range(0,32)]
+    i.regfile[15]=[0b00000011 for x in range(0,32)]
+    i.step()
+    #i.regfile[15]=[0b11000000 for x in range(0,32)]
+    i.regfile[15]=[0b11111100 for x in range(0,32)]
+    i.step()
+    i.step()
+    print ([np.binary_repr(n, width=8) for n in i.regfile[0][0:4]])
+    sigmazero_three=i.regfile[0][8:12]
+    
     
     
 def sigma_zero1():
@@ -567,5 +626,5 @@ def test1():
 if __name__ == '__main__':
     #main()
     #test()
-    sigma_zero()
-    #sigma_one()
+    #sigma_zero()
+    sigma_one()
